598dc21e9cdac7dfb95992708866b9272df66398
clement.hector <clement.hector@fixstudio.com>
2023-04-25 20:44:07 +0200

    add docstring

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -1,24 +1,33 @@
 
 
 def get_winner(tuple: list) -> bool:
+    """Returns the winner if there is one, else returns None
+    """
     if tuple[0] == tuple[1] == tuple[2] and tuple[0] != 0:
         return tuple[0]
 
 def is_unfinished(tuple_board: list) -> bool:
-    return True if tuple_board.count(0) > 0 else False
+    """Returns True if there are any empty cells, else returns False"""
+    return tuple_board.count(0) > 0
 
 def get_rows(board: list) -> list:
+    """Returns a list of tuples representing the rows of the board"""
     return [tuple(row) for row in board]
 
 def get_columns(board: list) -> list:
+    """Returns a list of tuples representing the columns of the board"""
     return list(zip(*board))
 
 def get_diagonals(board: list) -> list:
+    """Returns a list of tuples representing the diagonals of the board"""
     diag1 = [board[i][i] for i in range(3)]
     diag2 = [board[i][2-i] for i in range(3)]
     return [diag1, diag2]
 
 def check_board(board: list) -> int:
+    """Returns 1 if player 1 wins, 2 if player 2 wins, 0 if it's a tie,
+    and -1 if the game is unfinished
+    """
     rows = get_rows(board)
     cols = get_columns(board)
     diags = get_diagonals(board)
